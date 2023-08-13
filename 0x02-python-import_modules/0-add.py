8d0098dba86942f532d7e9777d2740ec541ccb45
realFrankieWilson <frankuwill101@gmail.com>
2023-08-13 21:45:23 +0100

    hot fix two files

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,5 +4,6 @@ if __name__ == "__main__":
     from add_0 import add
 
     a = 1
-    b = 2
+    b = w
+
     print('{} + {} = {}'.format(a, b, add(a, b)))
--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     a = 10
     b = 5
 
-    print('{} + {} = {}'.format(a, b, a + b))
-    print('{} - {} = {}'.format(a, b, a - b))
-    print('{} * {} = {}'.format(a, b, a * b))
-    print('{} / {} = {}'.format(a, b, a // b))
+    print('{} + {} = {}'.format(a, b, add(a, b)))
+    print('{} - {} = {}'.format(a, b, add(a, b)))
+    print('{} * {} = {}'.format(a, b, add(a, b)))
+    print('{} / {} = {}'.format(a, b, add(a, b)))
