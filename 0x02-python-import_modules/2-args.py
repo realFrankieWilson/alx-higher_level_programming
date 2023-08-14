fc68d78dc40665f305ed74e693bb327a6b280d63
realFrankieWilson <frankuwill101@gmail.com>
2023-08-14 09:25:13 +0100

    hot fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
+    import sys
 
 a = 0
-agv_len = len(argv) - 1
+agv_len = len(sys.argv) - 1
 if agv_len == 1:
     print('{} argument:'.format(agv_len))
-    print('{}: {}'.format(agv_len, argv[1]))
+    print('{}: {}'.format(agv_len, sys.argv[1]))
 elif agv_len == 0:
     print('{} arguments.'.format(agv_len))
 else:
     print('{} arguments:'.format(agv_len))
     while a < agv_len:
-        print('{}: {}'.format(a + 1, argv[a + 1]))
+        print('{}: {}'.format(a + 1, sys.argv[a + 1]))
         a += 1
