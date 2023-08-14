a6aaa5477a1eea5c8615e66b3d6f35764d2e04c5
realFrankieWilson <frankuwill101@gmail.com>
2023-08-14 10:11:03 +0100

    hot fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,6 @@
 if __name__ == "__main__":
     import sys
 
-a = 0
 agv_len = len(sys.argv) - 1
 if agv_len == 0:
     print('0 arguments.')
@@ -10,6 +9,9 @@ elif agv_len == 1:
     print('1 argument:')
 else:
     print('{} arguments:'.format(agv_len))
+    '''a = 0
     while a < agv_len:
         print('{}: {}'.format(a + 1, sys.argv[a + 1]))
-        a += 1
+        a += 1'''
+    for i in range(agv_len):
+        print('{}: {}'.format(i + 1, sys.argv[i + 1]))
