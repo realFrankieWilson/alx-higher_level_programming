2501deba72a691971c5a0cd3691acbf19e0d3b25
realFrankieWilson <frankuwill101@gmail.com>
2023-08-14 10:12:30 +0100

    hot fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,9 +9,7 @@ elif agv_len == 1:
     print('1 argument:')
 else:
     print('{} arguments:'.format(agv_len))
-    '''a = 0
+    a = 0
     while a < agv_len:
         print('{}: {}'.format(a + 1, sys.argv[a + 1]))
-        a += 1'''
-    for i in range(agv_len):
-        print('{}: {}'.format(i + 1, sys.argv[i + 1]))
+        a += 1
