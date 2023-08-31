c436cf758e20ec78962e8dfeae342b987f8b839f
realFrankieWilson <frankuwill101@gmail.com>
2023-08-31 17:44:20 +0100

    hot fix

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 # 1-square.py By Franke Williams Ugwu
+""" Defines a Square """
 
 
 class Square:
-    """
-    class with private atributte
-    """
+    """ Represents a Square class """
     def __init__(self, __size):
+        """Initializes the square with with one argument
+        Args: size -> Represents the size of the squre defined
+        """
         self.__size = __size
