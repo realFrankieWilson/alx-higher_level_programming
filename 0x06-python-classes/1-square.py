9cad5cbd027a5bd166a56a9889551a24edee0428
realFrankieWilson <frankuwill101@gmail.com>
2023-08-31 17:38:38 +0100

    hot fix

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 class Square:
     """
-    class with private attributes
+    class with private atributte
     """
     def __init__(self, __size):
         self.__size = __size
