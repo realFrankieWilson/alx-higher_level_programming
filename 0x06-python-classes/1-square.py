19f8f159483224d8f0ab67ddbc2d1f29c0f092ab
realFrankieWilson <frankuwill101@gmail.com>
2023-08-31 17:25:41 +0100

    added: my_square.size file

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+class Square:
+    def __init__(self, size):
+        """Initializes the square class
+        Args: size -> represents the size of the square
+        """
+        self.__size = size
