898bacda292603c5f551a0223f8596f54e86a6c9
realFrankieWilson <frankuwill101@gmail.com>
2023-09-22 18:29:07 +0100

    hot fix

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -12,10 +12,10 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """ A constructor, initializes the class objects """
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
         super().__init__(id)
 
     # Lists of functions that get the class properties
