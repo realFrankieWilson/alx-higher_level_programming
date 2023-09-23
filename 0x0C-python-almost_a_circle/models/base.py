5e9e7b6165881622f59fee8d6c29c99061fd5ddc
realFrankieWilson <frankuwill101@gmail.com>
2023-09-23 01:49:07 +0100

    updated base.py to 16 and 17

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -25,3 +25,30 @@ class Base:
         if not list_dictionaries:
             return '[]'
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        A class method that writes the JSON string representation of a list
+        object
+        """
+        file_n = '{}.json'.format(cls.__name__)
+        if not list_objs:
+            list_objs = []
+
+        with open(file_n, 'w') as f:
+            f.write(cls.to_json_string(
+                [cls.to_dictionary(x) for x in list_objs]))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """ Returns list of json string representations """
+        json_string_lst = []
+
+        if not json_string_lst and not '':
+            if type(json_string) is not str:
+                raise TypeError('json_string must be a string')
+
+            json_string_lst = json.loads(json_string)
+
+        return json_string_lst
