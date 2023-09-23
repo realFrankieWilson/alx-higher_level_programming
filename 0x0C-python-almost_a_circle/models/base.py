11dba8ae29ff2f4827849a8b5100c9963929a737
realFrankieWilson <frankuwill101@gmail.com>
2023-09-23 01:12:18 +0100

    added json dictionary

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # base.py
 """ The base class module """
+import json
 
 
 class Base:
@@ -17,3 +18,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ A static function of a class that returns a json string """
+        if not list_dictionaries:
+            return '[]'
+        return json.dumps(list_dictionaries)
