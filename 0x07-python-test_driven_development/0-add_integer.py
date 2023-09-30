7be6c512c185b89f41184fad2cb24f4f790ba5fe
realFrankieWilson <frankuwill101@gmail.com>
2023-09-30 02:06:37 +0100

    hot fix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -2,32 +2,23 @@
 """
 The module is about adding two integers
 The 0-add_integer module supplies one function, add_integer(a, b).
+and it only takes in integer or float
 """
 
 
 def add_integer(a, b=98):
-    """ Return sum of two integers or floats
-
-
+    """
+    Adds a to b, (both integers ) just checking
     Args:
-        a is the first argument
-        b is the second argumet
-
-    Returns:
-        sum of a and b
-
-
-    Raises:
-        TypeError: if the arguments are not int or float
-    Rasise:
-        TypeError: if the arguments are not float or int
-
-
+        a -> is an int;
+        b -> is an int;
     """
 
     if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
-    if ((not isinstance(b, int) and not isinstance(b, float))):
+
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+
         raise TypeError("b must be an integer")
 
     return (int(a) + int(b))
