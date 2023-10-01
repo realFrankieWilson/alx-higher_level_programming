a35bc1793188f84dabb527299edcd3309154c5fa
realFrankieWilson <frankuwill101@gmail.com>
2023-10-01 01:25:50 +0100

    added file 2

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,43 +1,50 @@
 #!/usr/bin/python3
+# 2-matrix_divided.py
 """
-The module is about dividing a matrix
-
+Module that divides all elements of a matrix.
 """
 
 
 def matrix_divided(matrix, div):
     """
-    The function divides all elements of a matrix by a given
-    number.
-
+    Divides elements of a matrix
     Args:
-     matrix: A list of lists (matrix)- members can be of type ints or floats
-     div: Number to be used for the division (can be a float or an integer)
-
-    Raises:
-        TypeError: If the matrix contains non-numbers
-        TypeError: If the matrix contains rows of different sizes
-        TypeError: If div is not an int or float
-        ZeroDivisionError: If div is 0
-
-    Returns:
-        A new matrix after the division
-
+        matrix -> (first parameter) Must be lists of integers or float,
+            Otherwise raises a TypeError.
+            matrix must be a matrix (See the test file for full documentation)
+        div -> (Second argument) Must be a number. Otherwise, raises a
+            TypeError. It must be > 0.
 
     """
-    if (not isinstance(matrix, list) or matrix == [] or
-            not all(isinstance(row, list) for row in matrix) or
-            not all((isinstance(elem, int) or isinstance(elem, float))
-                    for elem in [digit for row in matrix for digit in row])):
-        raise TypeError("matrix must be a matrix (list of lists) of "
-                        "integers/floats")
 
-    if not all(len(row) == len(matrix[0]) for row in matrix):
+    # Checks if matrix is a matrix
+    check_if_matrix = all(isinstance(ele, list) for ele in matrix)
+
+    """ The error messages to be displayed """
+    error_msg = "matrix must be a matrix (list of lists) of integers/floats"
+
+    """ The error coditions """
+    # Checks matrix: list of lists
+    if (check_if_matrix) is False:
+        raise TypeError(error_msg)
+
+    # Checks for string in the lists
+    for row in matrix:
+        for ele in row:
+            if not (isinstance(ele, int) or isinstance(ele, float)):
+                raise TypeError(error_msg)
+
+    # Checks for list lengths:
+    if not all(map(lambda init_len: len(matrix[0]) == len(init_len), matrix)):
         raise TypeError("Each row of the matrix must have the same size")
 
+    # Checks for division types:
     if not isinstance(div, int) and not isinstance(div, float):
         raise TypeError("div must be a number")
 
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
+
+    # The Output:
+    res = [list(map(lambda num: round(num / div, 2), ele)) for ele in matrix]
+    return res
