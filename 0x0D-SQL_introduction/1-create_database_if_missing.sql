200c1202393a94f1ddd16db52e9406a5260f30e3
frankie wilson <frankuwill101@gmail.com>
2023-10-18 10:11:43 -0700

    added tastk 1

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,5 @@
+-- Creates the database hbtn_0c_0 in MySQL server.
+--	if database hbtn_0c_0 already exitst, script should not fail
+--	The SELECT or SHOW statements are not allowed.
+CREATE DATABASE  IF NOT EXISTS
+hbtn_0c_0;
