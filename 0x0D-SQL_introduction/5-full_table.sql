1e698baf279f163df19e324919da2d01a38249ac
frankie wilson <frankuwill101@gmail.com>
2023-10-18 13:32:27 -0700

    hot fix file 5

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,8 +1,4 @@
 -- Prints the full description of the table first_table from the database
 --	Usage of DESCRIBE or EXPLAIN statement are not allowed
--- SHOW CREATE TABLE
--- hbtn_0c_0.first_table
-SELECT column_name, column_type, is_nullable, column_default
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0'
-	AND TABLE_NAME = 'first_table';
+SHOW CREATE TABLE
+first_table
