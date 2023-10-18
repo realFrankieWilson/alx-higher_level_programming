e76e5fcb99fde237285c2d9130f752cac63ab4cb
realFrankieWilson <frankuwill101@gmail.com>
2023-10-18 18:38:14 +0100

    List tables

---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,5 @@
+-- lists all the tables of a database in MySQL server.
+--	The database name will be passed as argument of mysql command.
+--	(i.e, mysql is the name of the databse)
+USE mysql;
+SHOW TABLES;
