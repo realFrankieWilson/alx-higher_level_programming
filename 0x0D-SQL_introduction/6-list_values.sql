98cb7fb577843dcb6d2f1e93692d6252cfd33b43
frankie wilson <frankuwill101@gmail.com>
2023-10-18 13:33:57 -0700

    fixe last checker

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,4 +1,4 @@
 -- Lists all rows of the table first_table from the database hbtn_0c_0
 -- in MySql server.
 --		all fields should be printed.
-SELECT * FROM hbtn_0c_0.first_table;
+SELECT * FROM first_table;
