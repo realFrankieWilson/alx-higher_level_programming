e7b0bb25557b603afc6a44fd8fed3bf90f323e62
frankie wilson <frankuwill101@gmail.com>
2023-10-22 04:45:18 -0700

    added more files

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- Creates the database by the name hbtn_0d_usa and the table states
+--	(in the database hbtn_0d_usa) on MySQL server.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(
+	id INT UNIQUE PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(256) NOT NULL
+	);
