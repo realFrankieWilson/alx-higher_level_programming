4cc0cfb7fa71b59588837a3f25aee3a890c2651e
frankie wilson <frankuwill101@gmail.com>
2023-10-22 04:05:55 -0700

    added more files

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,4 @@
+-- Creates a table by the name force_name on Mysql server.
+CREATE TABLE IF NOT EXISTS force_name(
+	id INT,
+	name VARCHAR(255) NOT NULL);
---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,5 @@
+-- Creates a table with a name unique_id on MySQL server.
+CREATE TABLE IF NOT EXISTS unique_id(
+	id INT UNIQUE DEFAULT 1,
+	name VARCHAR(256)
+	);
