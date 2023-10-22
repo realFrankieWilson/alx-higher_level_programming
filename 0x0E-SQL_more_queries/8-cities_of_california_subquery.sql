ac8b7be1149b0ec38a2565457d1617d1690d324e
frankie wilson <frankuwill101@gmail.com>
2023-10-22 05:23:08 -0700

    hot fix

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,8 @@
 -- lists all the cities fo California that can be found in the database
 --	hbtn_0d_usa.
-SELECT id, name from cities
+SELECT id, name
+FROM cities
 WHERE state_id = (
 	SELECT id FROM states WHERE name = 'California'
-);
+)
+ORDER BY cities.id ASC;
