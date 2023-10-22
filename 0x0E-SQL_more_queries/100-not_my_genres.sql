dd1deffcd2675a2cd22fec0d448eb2d5600769eb
frankie wilson <frankuwill101@gmail.com>
2023-10-22 06:34:45 -0700

    advanced 100

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,13 @@
+-- lists all genres of the show not linked to the sow Dexter using 
+--	the hbtn_0d_tvsows database.
+SELECT tv_genres.name FROM tv_genres
+WHERE tv_genres.id NOT IN(
+	SELECT tv_genres.id
+	FROM tv_genres
+	INNER JOIN tv_show_geners
+	ON tv_genres.id = tv_show_genres.genre_id
+	INNER JOIN tv_shows
+	ON tv_show_genres.show_id = tv_shows.id
+	WHERE tv_shows.title = "Dexter"
+)
+ORDER BY tv_genres.name;
