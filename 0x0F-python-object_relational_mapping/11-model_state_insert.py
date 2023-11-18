3b16a75d111df538ad59537e15ff7ab19de5d09c
realFrankieWilson <frankuwill101@gmail.com>
2023-11-18 23:37:53 +0100

    added 3 files

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''
+A script that changes the name object from database hbtn_0e_6_usa
+'''
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+from model_state import Base, State
+
+
+if __name__ == '__main__':
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                argv[1], argv[2], argv[3])
+            )
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
+
+    states = session.query(State).filter(State.id == 2).first()
+    states.name = 'New Mexico'
+    session.commit()
+
+    session.close()
