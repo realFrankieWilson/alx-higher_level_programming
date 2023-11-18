3f94c61408f27273d2300ac066450e7d15ab0d09
realFrankieWilson <frankuwill101@gmail.com>
2023-11-18 16:24:20 +0100

    modified file 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,31 +1,25 @@
 #!/usr/bin/python3
-""" Lists all cities from the database hbtn_0e_4_usa """
-import MySQLdb
+''' Lists all cities from the hbtn_0e_4_usa database'''
+import MySQLdb as msd
 from sys import argv
 
 
 def mysqlcon():
     conn = None
-    conn = MySQLdb.connect(
-            host="localhost",
+    conn = msd.connect(
             user=argv[1],
             passwd=argv[2],
             db=argv[3],
             port=3306
             )
 
-    cur = conn.cursor()
-
-    cur.execute("SELECT cities.id, cities.name, states.name \
-            FROM cities JOIN states ON cities.state_id \
-            ORDER BY cities.id ASC")
-
-    cities = cur.fetchall()
-
-    for city in cities:
+    curs = conn.cursor()
+    curs.execute("SELECT cities.id, cities.name, states.name \
+            FROM cities JOIN states ON cities.state_id = states.id")
+    for city in curs.fetchall():
         print(city)
 
-    cur.close()
+    curs.close()
     conn.close()
 
 
