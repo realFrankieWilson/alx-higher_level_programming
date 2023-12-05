4bd2293c64769b9038b32b7809d767cfe1c1b542
realfrankiewilson <frankuwill101@gmail.com>
2023-12-04 18:45:11 -0800

    hot fix

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # A script that prints the lenght of the a url request.
-resp=$(curl -s -w "%{size_download}" -o /dev/null "$1") && echo "$resp"
+curl -Is "$1" | grep Content-Length: | cut -d ' ' -f2
